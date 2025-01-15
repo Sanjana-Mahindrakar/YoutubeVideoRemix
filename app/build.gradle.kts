512cd8beea0f46ca9f0c79a60fb20d98e081dc48
Sanjana-Mahindrakar <sanj.mahindrakar@gmail.com>
2025-01-15 16:36:54 +0530

    Update build.gradle.kts file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,37 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
 
+android {
+    namespace = "com.example.videoplayer1"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.videoplayer1"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.leanback)
+    implementation(libs.glide)
+}
